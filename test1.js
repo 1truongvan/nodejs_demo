cf1e7c125c3c4e58321e832f16f81b793297d8bd
1truongvan <truongvantruong4321@gmail.com>
2025-09-13 17:18:21 +0700

    mota

--- test1.js
+++ test1.js
@@ -1,6 +1,17 @@
-function showinfo(name, age) {
-    console.log(`Teen: ${name}, Tuoi: ${age}`)
-
+// Hàm hiển thị thông tin
+function showInfo(name, age) {
+  if (!name) name = "Unknown";
+  if (!age) age = "Chưa rõ";
+  console.log(`Tên: ${name}, Tuổi: ${age}`);
 }
 
-showinfo("truong", 20);
+// Lấy tham số từ dòng lệnh
+// Cú pháp chạy: node app.js "truong" 20
+const args = process.argv.slice(2);
+
+if (args.length >= 2) {
+  showInfo(args[0], args[1]);
+} else {
+  // Nếu không có tham số, gọi mặc định
+  showInfo("Truong", 20);
+}
